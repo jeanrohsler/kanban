880126a7bc665b907648db4c3112b6301257a8d7
jeanrohsler <jeanpaulorohsler@gmail.com>
2023-04-19 16:03:49 -0300

    angular/kanban-fire/

---
+++ angular/kanban-fire/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-6e8c2',
+    appId: '1:987949834832:web:2d4250a8b861ac008b2755',
+    databaseURL: 'https://kanbanfire-6e8c2-default-rtdb.firebaseio.com',
+    storageBucket: 'kanbanfire-6e8c2.appspot.com',
+    apiKey: 'AIzaSyCZZCjEuRI9557RPRB2meCV0hmEuFw4m6Y',
+    authDomain: 'kanbanfire-6e8c2.firebaseapp.com',
+    messagingSenderId: '987949834832',
+    measurementId: 'G-3NCTP4QQFD',
+  },};
